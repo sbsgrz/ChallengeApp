5740fe8b6af57f6b347b04236ead432368fa836b
sbsgrz <sebastian.grzeszczak@hotmail.com>
2023-02-25 18:41:18 +0100

    Dodaj pliki projektów.

---
+++ ChallengeApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+var varInt = int.MaxValue;
+var varUInt = uint.MaxValue;
+
+var varLong = long.MaxValue;
+var varULong = ulong.MaxValue;
+
+var varFloat = float.MaxValue;
+var varDouble = double.MaxValue;
+
+var varString = string.Empty;
+varString = "Oto String";
+
+Console.WriteLine("varInt = " + varInt);
+Console.WriteLine("varUInt = " + varUInt);
+Console.WriteLine("varLong = " + (long)varLong);
+Console.WriteLine("varULong = " + varULong);
+Console.WriteLine("varFloat = " + varFloat);
+Console.WriteLine("varDouble = " + varDouble);
+Console.WriteLine("varString = " + varString);
+Console.WriteLine("String + Int: " + varString + varInt);
+
+Console.ReadLine();
