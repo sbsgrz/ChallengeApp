9cdffed6a744ae895ac4191ad82a5f7690997c6a
sbsgrz <sebastian.grzeszczak@hotmail.com>
2023-02-26 15:36:45 +0100

    Największa liczba - v1

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,21 +1,45 @@
-﻿int number = 0;
-int[] rejestr  = new int[10];
+﻿// maksymalna liczba z 3
 
-for (int i = 0; i < 10; i++)
-    rejestr[i] = 0;
+int[] tab = new int[3];
+int max = 0;
 
-Console.WriteLine("Podaj liczbę");
-number = Convert.ToInt32(Console.ReadLine());
 
-while(number > 0)
+Console.WriteLine("Podaj pierwszą liczbę");
+tab[0] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Podaj drugą liczbę");
+tab[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Podaj trzecią liczbę");
+tab[2] = Convert.ToInt32(Console.ReadLine());
+
+if (tab[0] > tab[1])
 {
-    rejestr[number % 10]++;
-    number /= 10;
+    if (tab[0] > tab[2])
+        Console.WriteLine("Największa jest liczba " + tab[0]);
+    else if (tab[2] > tab[0])
+        Console.WriteLine("Największa jest liczba " + tab[2]);
+    else
+        Console.WriteLine("Liczby 1 i 3 (" + tab[0] + ") są największe i równe.");
 }
-
-for (int i = 0; i < 10; i++)
+else if (tab[1] > tab[0])
 {
-    Console.WriteLine(i + " => " + rejestr[i]);
+    if (tab[1] > tab[2])
+        Console.WriteLine("Największa jest liczba " + tab[1]);
+    else if (tab[2] > tab[1])
+        Console.WriteLine("Największa jest liczba " + tab[2]);
+    else
+        Console.WriteLine("Liczby 2 i 3 (" + tab[1] + " są największe i równe.");
 }
-
-Console.ReadLine();
+else if (tab[0] > tab[2])
+{
+    Console.WriteLine("Liczby 1 i 2 (" + tab[0] + ") są największe i równe.");
+}
+else if (tab[2] > tab[0])
+{
+    Console.WriteLine("Największa jest liczba " + tab[2]);
+}
+else
+{
+    Console.WriteLine("Wszystkie liczby są równe");
+}
