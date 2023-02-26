db210338dceff1cfcc3918a379959c90ec88a234
sbsgrz <sebastian.grzeszczak@hotmail.com>
2023-02-26 16:02:20 +0100

    Max z 3, v3 - na listach

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,34 +1,27 @@
 ﻿// maksymalna liczba z 3
 
 int ilosc = 3;
-int[] tab = new int[ilosc];
+List<int> list = new List<int>();
 int max;
-int ilosc_max = 1;
-
+int ilosc_max = 0;
 
 Console.WriteLine("Podaj pierwszą liczbę");
-tab[0] = Convert.ToInt32(Console.ReadLine());
+list.Add(Convert.ToInt32(Console.ReadLine()));
 
 Console.WriteLine("Podaj drugą liczbę");
-tab[1] = Convert.ToInt32(Console.ReadLine());
+list.Add(Convert.ToInt32(Console.ReadLine()));
 
 Console.WriteLine("Podaj trzecią liczbę");
-tab[2] = Convert.ToInt32(Console.ReadLine());
+list.Add(Convert.ToInt32(Console.ReadLine()));
 
-max = tab[0];
-for (int i = 1; i < ilosc; i++)
-    if (tab[i] > max)
-    {
-        max = tab[i];
-        ilosc_max = 1;
-    }
-    else if (tab[i] == max)
-    {
+max = list.Max();
+
+foreach (int i in list)
+    if (list[i] == max)
         ilosc_max++;
-    }
 
+Console.WriteLine("Maksymalna liczba to: " + list.Max() + ". Jest " + ilosc_max + " takich liczb.");
 
-Console.WriteLine("Maksymalna liczba to: " + max + ". Jest " + ilosc_max + " takich liczb.");
 
 Console.ReadLine();
 
