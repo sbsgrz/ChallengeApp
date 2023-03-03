3c76b1c5f0b52db04d9bba337d371917004c0a3d
sbsgrz <sebastian.grzeszczak@hotmail.com>
2023-03-03 21:39:13 +0100

    Dzień 8 - praca domowa

--- ChallengeApp.Tests/TypeTests.cs
+++ ChallengeApp.Tests/TypeTests.cs
@@ -2,14 +2,31 @@
 {
     internal class TypeTests
     {
-        // arrange
+        [Test]
+        public void Test()
+        {
+            // arrange
+            var emploee1 = new Emploee("Jan", "Kowalski", 20);
+            var emploee2 = new Emploee("Jan", "Kowalski", 20);
 
+            // act
 
-        // act
+            // assert
+            Assert.AreNotEqual(emploee1,emploee2);
+        }
 
+        [Test]
+        public void Test2() 
+        {
+            // arrange
+            var emploee1 = new Emploee("Jan", "Kowalski", 20);
+            var emploee2 = emploee1;
 
-        // assert
+            //act
 
+            // arrange
+            Assert.AreEqual(emploee1, emploee2);
 
+        }
     }
 }
