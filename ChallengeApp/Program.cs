f5093d3f611308efb7e7c7c553876477fdbe64dc
sbsgrz <sebastian.grzeszczak@hotmail.com>
2023-03-09 07:48:27 +0100

    Dzień 9 - konwersja testy

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -9,30 +9,30 @@ List<Emploee> listEmploee = new(){ e1, e2, e3};
 
 
 Random r = new();
-int grades;
+int grade;
 foreach (var e in listEmploee)
 {
     for (int i = 0; i < 10; i++)
     {
-        grades = r.Next(100);
+        grade = r.Next(100);
         
         if(i % 10 == 0)
-            grades = -grades;
+            grade = -grade;
 
-        if(e.AddGrades(grades))
+        if(e.AddGrades(grade))
         {
-            Console.WriteLine($"Dodano {grades} punktów pracownikowi: {e.FirstName} {e.LastName}");
+            Console.WriteLine($"Dodano {grade} punktów pracownikowi: {e.FirstName} {e.LastName}");
         }
-        else if(!e.AddGrades(grades))
+        else if(!e.AddGrades(grade))
         {
-            // Console.WriteLine("BŁĄD!!! Próbujesz dodać liczbę ujemną - użyj funkcji RemoveGrades()!");
+            // Console.WriteLine("BŁĄD!!! Próbujesz dodać liczbę ujemną - użyj funkcji Removegrade()!");
             Console.WriteLine($"Pracownik {e.FirstName} {e.LastName} funkcja AddGrade - podana wartość jest poza skalą");
         }
 
         /*
-            if(e.RemoveGrades(grades))
+            if(e.Removegrade(grade))
             {
-                Console.WriteLine("Odjęto " + grades + " punktów pracownikowi: " + e.FirstName + " " + e.LastName);
+                Console.WriteLine("Odjęto " + grade + " punktów pracownikowi: " + e.FirstName + " " + e.LastName);
             }
             else
             {
@@ -47,9 +47,47 @@ var theWinner = new Emploee();
 
 foreach(var e in listEmploee)
 {
-    Console.WriteLine(e.FirstName+ " " + e.LastName + " - grades: " + e.GetStatistics().Sum);
+    Console.WriteLine(e.FirstName+ " " + e.LastName + " - grade: " + e.GetStatistics().Sum);
     if (e.GetStatistics().Sum > theWinner.GetStatistics().Sum)
         theWinner = e;
 }
 Console.WriteLine();
-Console.WriteLine("And the winner is: " + theWinner.FirstName + " " + theWinner.LastName + " - grades: " + theWinner.GetStatistics().Sum);
+Console.WriteLine("And the winner is: " + theWinner.FirstName + " " + theWinner.LastName + " - grade: " + theWinner.GetStatistics().Sum);
+
+/*
+char
+sbyte
+byte
+short
+ushort
+int
+uint
+long
+ulong
+decimal
+double
+float 
+*/
+
+char gradeChar = '1';
+sbyte gradeSbyte = 1;
+byte gradeByte = 1;
+short gradeShort = 1;
+ushort gradeUshort = 1;
+int gradeInt = 1;
+uint gradeUint = 1;
+long gradeLong = 1;
+ulong gradeULong = 1;
+decimal gradeDecimal = 1;
+double gradeDouble = 1;
+
+Emploee emploeeTesty = new Emploee();
+
+Console.WriteLine("Testy różnych typów");
+emploeeTesty.AddGrades(gradeChar);
+emploeeTesty.AddGrades(gradeSbyte);
+emploeeTesty.AddGrades(gradeByte);
+emploeeTesty.AddGrades(gradeShort);
+emploeeTesty.AddGrades(gradeUshort);
+emploeeTesty.AddGrades(gradeInt);
+
