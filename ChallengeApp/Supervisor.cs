97f1df2c739def3820c94e335f2889b759cd19e0
sbsgrz <sebastian.grzeszczak@hotmail.com>
2023-03-31 20:15:38 +0200

    ?

---
+++ ChallengeApp/Supervisor.cs
@@ -0,0 +1,35 @@
+﻿namespace ChallengeApp
+{
+    public class Supervisor : IEmploee
+    {
+        public void AddGrade(float grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(string grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(char grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(double grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
